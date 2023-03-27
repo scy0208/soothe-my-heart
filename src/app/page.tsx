55847f8786486704aba9198df46adcbc6907c2b9
Chunyang Shen <cshen@cshen-macbookpro.local>
2023-03-27 15:57:28 -0700

    Responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,22 +67,31 @@ export default function Home() {
 
 
   return (
-
-    <div className="w-full h-screen flex flex-col items-center justify-center py-2 bg-vision bg-no-repeat bg-center bg-cover"> 
-      <div className="flex items-center bg-black/50 flex-col p-8 py-3 rounded-lg text-white" >         
-        <form onSubmit={handleSubmitStream} className="w-full max-w-screen-lg">
-          <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
-            <input type="text" value={text} onChange={(event) => setText(event.target.value)} className="appearance-none bg-transparent border-none w-full py-1 px-2 leading-tight focus:outline-none resize-none overflow-hidden break-all"
-  placeholder="Let go of the burden in your heart here. 在此放下你心中的重担吧"
-  size={Math.max(60, text.length + 1)} />
-            <button type="submit" className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded">Let go</button>
-          </div>
-        </form>
-        {displayText && 
+  <div className="w-full h-screen flex flex-col items-center bg-vision bg-no-repeat bg-center bg-cover grid grid-cols-8">
+    <div className="flex bg-black/50 flex-col p-8 py-3 rounded-lg text-white col-start-2 col-span-6 xl:col-start-3 xl:col-span-4">
+      <form onSubmit={handleSubmitStream} className="w-full max-w-screen-lg">
+        <div className="flex items-center border-b border-b-2 border-teal-500 py-2">
+          <input
+            type="text"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+            className="appearance-none bg-transparent border-none w-full py-1 px-2 leading-tight focus:outline-none resize-none overflow-hidden break-all"
+            placeholder="Write down the burden in your heart here. 在此放下你心中的重担吧"
+          />
+          <button
+            type="submit"
+            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+          >
+            Let go
+          </button>
+        </div>
+      </form>
+      {displayText && (
         <div className="flex items-center justify-center border-b-2 border-teal-500 w-full max-w-screen-lg text-2xl italic">
           <p>{displayText}</p>
-        </div>}
-      </div>
+        </div>
+      )}
     </div>
-  );
+  </div>
+);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'vision': "url('./static/DSC_1761.jpg')",
+        'vision': "url('./static/DSC_1761_S.jpg')",
       }
     },
   },
