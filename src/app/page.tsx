c7ce427fbd93844e44687b229dcae4ca56987842
Chunyang Shen <cshen@cshen-macbookpro.local>
2023-04-03 01:50:23 -0700

    Move GPT API to server side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@
 import { useState, useRef, useEffect} from 'react';
 import { SSE } from 'sse';
 
-import {callGptApi, getAPIkey} from './api/openaiAPI';
-
 export default function Home() {
   const [text, setText] = useState('');
   const [displayText, setDisplayText] = useState('');
@@ -15,55 +13,49 @@ export default function Home() {
     resultRef.current = displayText;
   }, [displayText]);
 
-  const handleSubmitStatic = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (typeof text === "undefined" || text === "") {
-      return;
-    }
-    const answer = await callGptApi(text);
-    setDisplayText(answer);
-    setText('');
-  };
-
   const handleSubmitStream = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setDisplayText('');
 
-    // Call OpenAI's GPT-3.5 API with a streaming response
-    const prompt = "Please use the language of the following content to generate a very gental and mild response to soothe the content provider by using stories or words from the Holy Bible"
-    let url = "https://api.openai.com/v1/chat/completions";
-    let key = getAPIkey();
-    let source = new SSE(url, {
+    if (!text) {
+      return;
+    }
+
+    const response = await fetch('/api/gpt-api', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${key}`
       },
-      payload: JSON.stringify({
-        model: "gpt-3.5-turbo-0301",
-        messages: [{"role": "user", "content": prompt}, {"role": "user", "content": text}],
-        stream: true,
-        max_tokens: 1000,
-        temperature: 0.9,
-        n: 1
-      })
+      body: JSON.stringify({
+        message: text
+      }),
     });
-    source.addEventListener("message", (e: MessageEvent) => {
-          if (e.data != "[DONE]") {
-            let payload = JSON.parse(e.data);
-            let text = payload.choices[0].delta.content;
-            if (typeof text != "undefined" && text != "\n") {
-              resultRef.current = resultRef.current + text;
-              if (typeof resultRef.current !== 'undefined') {
-                setDisplayText(resultRef.current);
-              }
-            }
-          } else {
-            source.close();
-          }
-        });
-    source.stream();
-  };
+
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = response.body
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+    let currentResponse: string[] = [];
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      resultRef.current = resultRef.current + chunkValue;
+      if (typeof resultRef.current !== 'undefined') {
+        setDisplayText(resultRef.current);
+      }
+    }
+  }
 
 
   return (
@@ -76,7 +68,7 @@ export default function Home() {
             value={text}
             onChange={(event) => setText(event.target.value)}
             className="appearance-none bg-transparent border-none w-full py-1 px-2 sm:px-0 leading-tight focus:outline-none resize-none overflow-hidden break-all text-xs md:text-s lg:text-base xl:text-xl"
-            placeholder="Write down the burden here. 在此放下你心中的重担吧"
+            placeholder="Write down your burden here. 在此放下你心中的重担吧"
           />
           <button
             type="submit"
