f08f47fbaed0c167917d06a8dc356e8efc131a72
Chunyang Shen <cshen@cshen-macbookpro.local>
2023-04-03 02:21:42 -0700

    Add missing files

---
+++ src/app/api/gpt-api/route.ts
@@ -0,0 +1,36 @@
+import { OpenAIStream, OpenAIStreamPayload } from '@/utils/OpenAIStream'
+
+type RequestData = {
+  message: string
+}
+
+export const runtime = 'edge'
+
+
+export async function GET(request: Request) {
+  return new Response('Hello, OPEN-AI!')
+}
+  
+export async function POST(request: Request) {
+  const { message } = (await request.json()) as RequestData;
+  if (!message) {
+    return new Response('No message in the request', { status: 400 })
+  }
+
+  const prompt = "Please use the language of the following content and generate a very gental and mild response to soothe the content provider by using stories or words from the Holy Bible "
+
+  const payload: OpenAIStreamPayload = {
+    model: 'gpt-3.5-turbo',
+    // model: `${currentModel}`,
+    messages: [{"role": "user", "content": prompt}, {"role": "user", "content": message}],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 700,
+    stream: true,
+    n: 1,
+  }
+  const stream = await OpenAIStream(payload)
+  return new Response(stream)
+}
